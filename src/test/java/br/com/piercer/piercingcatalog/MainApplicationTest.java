c2871aed6dce8714e7cd3646dc743b2b83c1188f
Benilsn <benilson.mtr@gmail.com>
2024-06-22 20:29:15 -0300

    project template

---
+++ src/test/java/br/com/piercer/piercingcatalog/MainApplicationTest.java
@@ -0,0 +1,13 @@
+package br.com.piercer.piercingcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = MainApplication.class)
+class MainApplicationTest {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
